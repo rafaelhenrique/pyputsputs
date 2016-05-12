f743d5158f2ce866d0f2d3d6e4bb6000f02a6c57
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2016-05-11 21:45:26 -0300

    Fix index of leds

--- run.py
+++ run.py
@@ -1,16 +1,23 @@
 # -*- coding: utf-8 -*-
 import time
+import sys
 
 import alsaaudio
 import audioop
 import pingo
 
-board = pingo.detect.get_board()
+try:
+    print("Loading board...")
+    board = pingo.detect.get_board()
+    print("Its ok...")
+except Exception as e:
+    print("Error on get_board: {}".format(e))
+    sys.exit(1)
 
 
-def configure_all_pins_out():
-    """Configure all pins of board on out mode"""
-    for pin in board.pins.values():
+def mode_pins_out(pins):
+    """Configure pins mode to out"""
+    for pin in pins:
         try:
             pin.mode = pingo.OUT
         except:
@@ -24,7 +31,15 @@ def blink(led, time_interval):
     led.low()
 
 
-def configure_microphone(rate=44100, channel=1, periodsize=160):
+def blink_leds(leds, time_interval):
+    """Hi leds by time"""
+    for led in leds:
+        led.hi()
+    for led in leds:
+        led.lo()
+
+
+def microphone(rate=44100, channel=1, periodsize=160):
     # Open the device in nonblocking capture mode. The last argument could
     # just as well have been zero for blocking mode. Then we could have
     # left out the sleep call in the bottom of the loop
@@ -46,25 +61,34 @@ def configure_microphone(rate=44100, channel=1, periodsize=160):
     return mic
 
 if __name__ == "__main__":
-    configure_all_pins_out()
     ordered_leds = [board.pins[11], board.pins[10], board.pins[9],
                     board.pins[6], board.pins[5], board.pins[3]]
+    mode_pins_out(ordered_leds)
 
-    maximum = 0
-    mic = configure_microphone()
+    mic = microphone()
     while True:
-        # Read data from device
         l, data = mic.read()
         if l:
-            # Return the maximum of the absolute value of all
-            # samples in a fragment.
-            led = audioop.max(data, 2) / 2100
+            # if wave of max is 32 this is works
+            # if not, you need recalibrate mic
+            wave = audioop.max(data, 2) / 1000
+            led_index = wave / 6.
 
-            # Only one adjust to my microphone
-            if led == 7:
-                led = 6
-            led -= 1
-            led = ordered_leds[led]
-            blink(led, .002)
-
-        time.sleep(.001)
+            if led_index > 5:
+                blink_leds(ordered_leds, .0001)
+                print("#"*6)
+            elif 5 > led_index > 4:
+                blink_leds(ordered_leds[0:5], .0001)
+                print("#"*5)
+            elif 4 > led_index > 3:
+                blink_leds(ordered_leds[0:4], .0001)
+                print("#"*4)
+            elif 3 > led_index > 2:
+                blink_leds(ordered_leds[0:3], .0001)
+                print("#"*3)
+            elif 2 > led_index > 1:
+                blink_leds(ordered_leds[0:2], .0001)
+                print("#"*2)
+            elif 1 > led_index > 0:
+                blink_leds(ordered_leds[0:1], .0001)
+                print("#"*1)
