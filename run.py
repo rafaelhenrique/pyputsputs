9283e755ab33f77bc76d7a35296d71133dc53f33
Onilton Maciel <oniltonmaciel@gmail.com>
2016-05-23 17:38:12 -0300

    Improve led_index calculation
    In the previous code, int(wave / 6.) + 1. The "magic number" 1 is not cool, 
    but the main problem is that we never get 0 for led_index, thus a bug.
    Using math.ceil also explains better what we are doing and fixes the bug

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import time
 import sys
+import math
 
 import alsaaudio
 import audioop
@@ -72,7 +73,7 @@ if __name__ == "__main__":
             # if wave of max is 32 this is works
             # if not, you need recalibrate mic
             wave = audioop.max(data, 2) / 1000
-            led_index = int(wave / 6.) + 1
+            led_index = int(math.ceil(wave / 6.))
 
             blink_leds(ordered_leds[0:led_index], .0001)
             print("#" * led_index)
